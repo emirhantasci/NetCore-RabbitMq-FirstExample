38b2a7d1b27f4f355b47f3d6008ddf365bac7e01
emirhantasci <73020659+emirhantasci@users.noreply.github.com>
2021-12-27 18:05:13 +0300

    RabbitMQ

---
+++ NetCoreRabbitMQExample/NetCoreRabbitMQExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace NetCoreRabbitMQExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello - RabbitMQ Start");
+            Customer customer = new Customer()
+            {
+                Name = "Emirhan",
+                Surname = "Taşçı"
+            };
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using(IConnection connection = factory.CreateConnection())
+            using(IModel channel = connection.CreateModel())
+            {
+                Console.WriteLine("RabbitMQ Queue");
+                channel.QueueDeclare(queue: "Test",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                string message = JsonConvert.SerializeObject(customer);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "Test",
+                    basicProperties: null,
+                    body: body);
+            }
+            Console.WriteLine("RabbitMQ Finish");
+            Console.ReadLine();
+        }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
